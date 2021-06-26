61d9b80ec3116653554cc368efe2d0cf086a9d7b
therock111 <coc10214@gmail.com>
2021-06-26 19:57:49 +0530

    errors

--- lib/authentication/registerscreen.dart
+++ lib/authentication/registerscreen.dart
@@ -114,6 +114,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                             'email': emailController.text,
                             'password': passwordController.text,
                             'uid': signedUser.user.uid,
+                            'profile':'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg',
                           });
                         });
                         Navigator.push(context,
